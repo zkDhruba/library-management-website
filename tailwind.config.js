dadccfe956f1209a7185ffe8dac082a1fc995255
Dhruba <zkdhruba@gmail.com>
2025-03-05 00:36:05 +0600

    tailwind issue solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 export default {
   darkMode: ["class", "string"],
@@ -51,11 +51,11 @@ export default {
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
         },
         primary: {
           DEFAULT: "#E7C9A5",
@@ -115,4 +115,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
