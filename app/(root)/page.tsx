398333cf8b70835e53450214b4ba77fb548f9672
Dhruba <zkdhruba@gmail.com>
2025-03-05 01:05:51 +0600

    Header section initiated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 const Home = () => (
   <>
---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Header = () => {
+  const pathname = usePathname();
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+      <ul>
+        <li>
+          <Link
+            href="library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
