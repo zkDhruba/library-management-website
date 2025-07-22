1938a182cf02d6653fe8274465adc0d2e9179670
Dhruba <zkdhruba@gmail.com>
2025-07-23 00:23:27 +0600

    postgres with drizzle connected in the project and user scema is created

---
+++ database/schema.ts
@@ -0,0 +1,37 @@
+import {
+  uuid,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+  pgEnum,
+  date,
+} from "drizzle-orm/pg-core";
+
+export const STATUS_ENUM = pgEnum("status", [
+  "PENDING",
+  "APPROVED",
+  "REJECTED",
+]);
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
+  "BORROWED",
+  "RETURNED",
+]);
+
+export const usersable = pgTable("users", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+  password: text("password").notNull(),
+  universityCard: text("university_card").notNull(),
+  status: STATUS_ENUM("status").default("PENDING"),
+  role: ROLE_ENUM("role").default("USER"),
+  lastActivityDate: date("last_activity_date").defaultNow(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+  }).defaultNow(),
+});
--- lib/config.ts
+++ lib/config.ts
@@ -6,6 +6,7 @@ const config = {
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
+    databaseUrl: process.env.DATABASE_URL!,
   },
 };
 
