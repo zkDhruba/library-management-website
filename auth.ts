d9c36c29df2252ad6d0e15b82a0a7a178c7d3f0b
Dhruba <zkdhruba@gmail.com>
2026-03-10 00:27:16 +0600

    auth js setup with authts

---
+++ auth.ts
@@ -0,0 +1,72 @@
+import NextAuth, { User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "./database/drizzle";
+import { users } from "./database/schema";
+import { eq } from "drizzle-orm";
+import { compare } from "bcryptjs";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const email = credentials.email as string | undefined;
+
+        if (!email) {
+          return null;
+        }
+
+        const user = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, email))
+          .limit(1);
+
+        if (!user || user.length === 0) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password.toString(),
+          user[0].password,
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return {
+          id: user[0].id.toString(),
+          email: user[0].email,
+          name: user[0].fullName,
+        } as User;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+
+      return session;
+    },
+  },
+});
