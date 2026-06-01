b26c129c0e1cb5e109671e07d5d08f4719c76edd
Dhruba <zkdhruba@gmail.com>
2026-06-01 23:45:57 +0600

    rate limitter set up done

--- lib/config.ts
+++ lib/config.ts
@@ -7,6 +7,10 @@ const config = {
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
     databaseUrl: process.env.DATABASE_URL!,
+    upstash: {
+      redisUrl: process.env.UPSTASH_REDIS_URL!,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+    },
   },
 };
 
---
+++ lib/ratelimit.ts
@@ -0,0 +1,11 @@
+import redis from "@/database/redis";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.fixedWindow(5, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
+export default ratelimit;
