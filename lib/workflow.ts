c1bbe07f0049ce32035d4b6c936b664c2347e08f
Dhruba <zkdhruba@gmail.com>
2026-06-02 23:37:55 +0600

    preparing deployment

---
+++ lib/workflow.ts
@@ -0,0 +1,7 @@
+import { Client as WorkflowClient } from "@upstash/workflow";
+import config from "./config";
+
+export const workflowClient = new WorkflowClient({
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
