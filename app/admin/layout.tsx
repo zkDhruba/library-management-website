e0e94bf502fb13c9a1cf01137cae3ec42b6c7058
Dhruba <zkdhruba@gmail.com>
2026-06-21 23:39:16 +0600

    admin panel layout roughly done

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 import "@/styles/admin.css";
 import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -11,7 +12,7 @@ const layout = async ({ children }: { children: ReactNode }) => {
     <main className="flex min-h-screen w-full flex-row">
       <Sidebar session={session} />
       <div className="admin-container">
-        <p>Header</p>
+        <Header session={session} />
         {children}
       </div>
     </main>
